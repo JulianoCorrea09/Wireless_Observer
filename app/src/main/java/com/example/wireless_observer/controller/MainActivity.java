22515f51599159405ca5048e3cd3afe2d69e7c0f
julianocorrea09 <julianocorrea09@gmail.com>
2020-06-15 09:01:00 -0300

    Add MVC

--- app/src/main/java/com/example/wireless_observer/controller/MainActivity.java
+++ app/src/main/java/com/example/wireless_observer/controller/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.wireless_observer;
+package com.example.wireless_observer.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.wireless_observer.R;
 
 public class MainActivity extends AppCompatActivity {
 
